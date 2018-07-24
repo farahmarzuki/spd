06640046204c994add4794bd9e188d60fa6141aa
Farah <farramarzuki@gmail.com>
2018-07-24 11:41:27 +0800

    create home page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller //controller tu parents UserController tu anak
+{
+    public function index() {
+    		return 'Hello World';
+    }
+
+    public function login() {
+
+    }
+}
