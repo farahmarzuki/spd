815ca1f5cde829ff3c04427a93ce06a1c66795f5
Farah <farramarzuki@gmail.com>
2018-07-25 11:56:43 +0800

    dah boleh masuk dalam databae

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller //controller tu parents UserController tu anak
 {
@@ -15,5 +16,26 @@ class UserController extends Controller //controller tu parents UserController t
     }
     public function register() {
     		return view('frontend.register');
+    }
+
+    public function registerPost(Request $request){
+    	$request->validate([
+    		'name' =>'required',
+    		'email' =>'required|email|unique:users',
+    		'ic' =>'required|unique:users',
+    		'password'=>'required'
+    	], ['name.required'=>'Sila masukkan nama',
+    		'email.email'=>'Email tidak sahkan',
+    		'email.unique'=>'Email sudah wujud'
+    	]);
+
+    	$user = new User (); //$user nama variable User Nama class
+    	$user->name =$request->name; //cara nak assign variable: dia request form data nama name
+    	$user->email=$request->email;//asign input field kt database
+    	$user->ic=$request->ic;
+    	$user->password=bcrypt($request->password);
+    	$user->save();
+
+    	return back()->with('success','Data anda dah masuk'); // with tu mcm session dia pegang sbb nak back balik
     }
 }
--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -5,29 +5,35 @@
     <section class="py-5">
       <div class="container">
       	<h1>Register New User<br></h1><br>
-      	<form>
+
+      	@include('common.alert')
+
+      	@include('common.form_error')
+
+      	<form method="POST" action="{{route('user.register.post')}}">
+      		@csrf
       	  <div class="form-group row">
       	    <label for="inputEmail3" class="col-sm-2 col-form-label">Nama</label>
       	    <div class="col-sm-10">
-      	      <input text="email"input type="email" class="form-control" id="inputEmail3" placeholder="Nama">
+      	      <input type="text" name="name" class="form-control" id="name" placeholder="Nama" value="{{old('name')}}">
       	    </div>
       	  </div>
       	  <div class="form-group row">
       	    <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
       	    <div class="col-sm-10">
-      	      <input text="password" input type="password" class="form-control" id="inputPassword3" placeholder="abc@email.com">
+      	      <input type="email" name="email" class="form-control" id="email" placeholder="abc@email.com" value="{{old('email')}}"> <!--untuk remembered kt form-->
       	    </div>
       	  </div>
       	  <div class="form-group row">
       	    <label for="inputPassword3" class="col-sm-2 col-form-label">IC no</label>
       	    <div class="col-sm-10">
-      	      <input text="icno" input type="icno" class="form-control" id="inputPassword3" placeholder="IC No without dash (-)">
+      	      <input type="text" name="ic" class="form-control" id="ic" placeholder="IC No without dash (-)" value="{{old('icno')}}">
       	    </div>
       	  </div>
       	  <div class="form-group row">
       	    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
       	    <div class="col-sm-10">
-      	      <input text="password" input type="password" class="form-control" id="inputPassword3">
+      	      <input type="password" name="password" class="form-control" id="inputPassword3">
       	    </div>
       	  </div>
       	    <div class="form-group row">
